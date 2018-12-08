a3e2d88dc654499197b8317da341701bcf8a99e4
waymobetta <jbroethke@gmail.com>
2018-12-07 16:05:56 -0800

    fix CORS for local testing

--- application.go
+++ application.go
@@ -3,8 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/handlers"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":5000", NewRouter()))
+	// log.Fatal(http.ListenAndServe(":5000", NewRouter()))
+
+	log.Fatal(http.ListenAndServe(":5000", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(NewRouter())))
 }
