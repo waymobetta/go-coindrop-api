16f2952dbeab3b94f26e6d5086724ba91e3a2c4f
waymobetta <jbroethke@gmail.com>
2018-12-07 16:06:17 -0800

    add signin and signup endpoints

--- auth.go
+++ auth.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -11,22 +10,35 @@ import (
 
 // signUp registers a user for coindrop
 func signUp(w http.ResponseWriter, r *http.Request) {
-	// Parse and decode the request body into a new `Credentials` instance
+	// initialize new Credentials struct object
 	creds := &Credentials{}
-	err := json.NewDecoder(r.Body).Decode(creds)
+
+	// Parse and decode the request body into a new `Credentials` instance
+	err := json.NewDecoder(r.Body).Decode(&creds)
 	if err != nil {
 		// If there is something wrong with the request body, return a 400 status
 		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, err.Error(), 400)
 		return
 	}
+
 	// Salt and hash the password using the bcrypt algorithm
 	// The second argument is the cost of hashing, which we arbitrarily set as 8 (this value can be more or less, depending on the computing power you wish to utilize)
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 8)
 
 	// Next, insert the username, along with the hashed password into the database
-	if _, err = Client.Query(`INSERT INTO coindropdbusers (email,password) VALUES ($1, $2)`, creds.Email, string(hashedPassword)); err != nil {
+	if _, err := Client.Query(`INSERT INTO coindropdbusers (email,password) VALUES ($1,$2)`, creds.Email, string(hashedPassword)); err != nil {
 		// If there is any issue with inserting into the database, return a 500 error
 		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	if err := json.NewEncoder(w).Encode(&creds); err != nil {
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
@@ -35,40 +47,54 @@ func signUp(w http.ResponseWriter, r *http.Request) {
 
 // signIn logs in a user for coindrop
 func signIn(w http.ResponseWriter, r *http.Request) {
-	// Parse and decode the request body into a new `Credentials` instance
+	// initialize new Credentials struct object
 	creds := &Credentials{}
+
+	// Parse and decode the request body into a new `Credentials` instance
 	err := json.NewDecoder(r.Body).Decode(creds)
 	if err != nil {
 		// If there is something wrong with the request body, return a 400 status
 		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, err.Error(), 400)
 		return
 	}
 	// Get the existing entry present in the database for the given username
-	result := Client.QueryRow(`SELECT password FROM coindropdbusers WHERE email=$1`, creds.Email)
+	row := Client.QueryRow(`SELECT password FROM coindropdbusers WHERE email=$1`, creds.Email)
 	if err != nil {
 		// If there is an issue with the database, return a 500 error
 		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 	// Create another instance of `Credentials` to store the credentials we get from the database
 	storedCreds := &Credentials{}
 	// Store the obtained password in `storedCreds`
-	err = result.Scan(&storedCreds.Password)
+	err = row.Scan(&storedCreds.Password)
 	if err != nil {
-		// If an entry with the username does not exist, send an "Unauthorized"(401) status
-		if err == sql.ErrNoRows {
+		if storedCreds.Email != creds.Email {
+			// If an entry with the username does not exist, send an "Unauthorized"(401) status
 			w.WriteHeader(http.StatusUnauthorized)
+			http.Error(w, err.Error(), 401)
 			return
 		}
-		// If the error is of any other type, send a 500 status
 		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
 	// Compare the stored hashed password, with the hashed version of the password that was received
-	if err = bcrypt.CompareHashAndPassword([]byte(storedCreds.Password), []byte(creds.Password)); err != nil {
+	if err := bcrypt.CompareHashAndPassword([]byte(storedCreds.Password), []byte(creds.Password)); err != nil {
 		// If the two passwords don't match, return a 401 status
 		w.WriteHeader(http.StatusUnauthorized)
+		http.Error(w, err.Error(), 401)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	if err := json.NewEncoder(w).Encode(creds); err != nil {
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
