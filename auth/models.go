6dfc052934722f65226db145625e57d4361b6929
waymobetta <jbroethke@gmail.com>
2018-12-17 14:51:11 -0800

    update models to include ID field

--- auth/models.go
+++ auth/models.go
@@ -2,6 +2,7 @@ package auth
 
 // Credentials is a struct of a user
 type Credentials struct {
+	ID       int    `json:"id"`
 	Password string `json:"password", db:"password"`
 	Email    string `json:"email", db:"email"`
 }
