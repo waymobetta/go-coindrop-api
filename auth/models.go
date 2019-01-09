445e0ab7a594db82f00809cdaa2deb30ac4308e8
waymobetta <jbroethke@gmail.com>
2019-01-08 18:59:29 -0800

    update struct property name

--- auth/models.go
+++ auth/models.go
@@ -2,6 +2,6 @@ package auth
 
 // Credentials is a struct of a user
 type Credentials struct {
-	ID     int    `json:"id", db:"id"`
-	UserID string `json:"user_id", db:"user_id"`
+	ID         int    `json:"id", db:"id"`
+	AuthUserID string `json:"auth_user_id", db:"auth_user_id"`
 }
