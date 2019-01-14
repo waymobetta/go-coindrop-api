047f545d515a1138a83f22e0209a60e165b81e8c
waymobetta <jbroethke@gmail.com>
2019-01-14 15:01:54 -0800

    remove models + add update wallet functionality

--- auth/auth.go
+++ auth/auth.go
@@ -3,6 +3,8 @@ package auth
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/waymobetta/go-coindrop-api/db"
@@ -12,77 +14,121 @@ import (
 // AddUserID adds an AWS cognito user ID to the coindrop_auth table
 func AddUserID(w http.ResponseWriter, r *http.Request) {
 	response := make(map[string]interface{})
-	// initialize new Credentials struct object
-	creds := &Credentials{}
+	// initialize new user struct object
+	user := new(db.User)
 
 	// Parse and decode the request body into a new `Credentials` instance
-	err := json.NewDecoder(r.Body).Decode(creds)
+	err := json.NewDecoder(r.Body).Decode(user)
 	if err != nil {
 		// If there is something wrong with the request body, return a 400 status
-		http.Error(w, err.Error(), 400)
+		response = utils.Message(false, "Could not decode JSON body")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Add("Content-type", "application/json")
+		utils.Respond(w, response)
 		return
 	}
 
 	// Next, insert the AWS cognito user ID into the coindrop_auth table
-	if _, err := db.Client.Query(`INSERT INTO coindrop_auth (auth_user_id) VALUES ($1)`, creds.AuthUserID); err != nil {
+	if _, err := db.Client.Query(`INSERT INTO coindrop_auth (auth_user_id) VALUES ($1)`, user.Info.AuthUserID); err != nil {
 		// If there is any issue with inserting into the database, return a 500 error
-		http.Error(w, err.Error(), 500)
+		response = utils.Message(false, "Could not add Reddit user to db")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Add("Content-type", "application/json")
+		utils.Respond(w, response)
 		return
 	}
-
-	if err := json.NewEncoder(w).Encode(&creds); err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	fmt.Println("[*] Successfully added user:", creds.AuthUserID)
 
 	response = utils.Message(true, "success")
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Add("Content-type", "application/json")
 	utils.Respond(w, response)
-	return
+
+	fmt.Printf("Successfully added user: %v\n\n", user.Info.AuthUserID)
 }
 
-// GetID gets a user id from their email
-func GetID(w http.ResponseWriter, r *http.Request) {
+// WalletUpdate handles updates to the wallet address for a user
+func WalletUpdate(w http.ResponseWriter, r *http.Request) {
 	response := make(map[string]interface{})
-	// initialize new Credentials struct object
-	creds := &Credentials{}
+	// initialize new variable user of User struct
+	user := new(db.User)
 
+	// add limit for large payload protection
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		response = utils.Message(false, "Error reading request body")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Add("Content-type", "application/json")
+		utils.Respond(w, response)
+		return
+	}
+	defer r.Body.Close()
+
+	// unmarshal bytes into user struct
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		response = utils.Message(false, "JSON Unmarshal error")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Add("Content-type", "application/json")
+		utils.Respond(w, response)
+		return
+	}
+
+	// update the user listing in db
+	_, err = db.UpdateWallet(user)
+	if err != nil {
+		response = utils.Message(false, "Could not update user wallet")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Add("Content-type", "application/json")
+		utils.Respond(w, response)
+		return
+	}
+
+	response = utils.Message(true, "success")
+	w.WriteHeader(http.StatusCreated)
+	utils.Respond(w, response)
+
+	fmt.Printf("Successfully updated wallet address for user: %v\n\n", user.Info.AuthUserID)
+}
+
+// GetWalletAddress gets a user's wallet address from their auth_user_id
+func GetWalletAddress(w http.ResponseWriter, r *http.Request) {
+	response := make(map[string]interface{})
+	// initialize new user struct object
 	user := new(db.User)
 
 	// Parse and decode the request body into a new `Credentials` instance
-	err := json.NewDecoder(r.Body).Decode(creds)
+	err := json.NewDecoder(r.Body).Decode(user)
 	if err != nil {
 		// If there is something wrong with the request body, return a 400 status
-		http.Error(w, err.Error(), 400)
+		response = utils.Message(false, "Could not decode JSON body")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Add("Content-type", "application/json")
+		utils.Respond(w, response)
 		return
 	}
 	// Get the existing entry present in the database for the given username
-	row := db.Client.QueryRow(`SELECT id FROM coindrop_auth WHERE auth_user_id=$1`, creds.AuthUserID)
+	row := db.Client.QueryRow(`SELECT wallet_address FROM coindrop_auth WHERE auth_user_id=$1`, user.Info.AuthUserID)
 	if err != nil {
 		// If there is an issue with the database, return a 500 error
-		http.Error(w, err.Error(), 500)
+		response = utils.Message(false, "Could not get user info")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Add("Content-type", "application/json")
+		utils.Respond(w, response)
 		return
 	}
-	// Store the obtained password in `storedCreds`
-	err = row.Scan(&user.Info.ID)
+	err = row.Scan(&user.Info.WalletAddress)
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		response = utils.Message(false, "Could not read rows from user info")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Add("Content-type", "application/json")
+		utils.Respond(w, response)
 		return
 	}
-
-	if err := json.NewEncoder(w).Encode(user.Info.ID); err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	fmt.Println("[*] Returned user id: ", user.Info.ID)
 
 	response = utils.Message(true, "success")
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Add("Content-type", "application/json")
 	utils.Respond(w, response)
-	return
+
+	fmt.Printf("Successfully returned wallet address for user: %v\n\n", user.Info.AuthUserID)
 }
--- auth/models.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-// Credentials is a struct of a user
-type Credentials struct {
-	ID         int    `json:"id", db:"id"`
-	AuthUserID string `json:"auth_user_id", db:"auth_user_id"`
-}
