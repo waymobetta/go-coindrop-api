06c18b6fdf08cdf71c441e5930ee5a01b7e66005
waymobetta <jbroethke@gmail.com>
2019-01-16 21:36:42 -0800

    add JWT middleware

--- router/router.go
+++ router/router.go
@@ -2,13 +2,28 @@ package router
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/waymobetta/go-coindrop-api/auth"
 	"github.com/waymobetta/go-coindrop-api/logger"
 )
 
+// NewRouter method creates a custom new mux router
 func NewRouter() *mux.Router {
+
+	region := os.Getenv("REACT_APP_AWS_COINDROP_COGNITO_REGION")
+	userPoolID := os.Getenv("REACT_APP_AWS_COINDROP_COGNITO_USER_POOL_ID")
+
+	su := &auth.ServiceUser{
+		Region:     region,
+		UserPoolID: userPoolID,
+	}
+
 	router := mux.NewRouter().StrictSlash(true)
+
+	router.Use(su.AuthMiddleware)
+
 	for _, route := range routes {
 		var handler http.Handler
 
