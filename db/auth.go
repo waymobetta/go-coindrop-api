e0a9cf6a891dca64267ed4f9b299dad3138a61ac
waymobetta <jbroethke@gmail.com>
2019-01-18 11:59:52 -0800

    add file for auth-table-related db writes

--- db/auth.go
+++ db/auth.go
@@ -2,6 +2,46 @@ package db
 
 // AUTH
 
+// AddUserID inserts an AWS cognito user ID to the coindrop_auth table
+func AddUserID(u *User) (*User, error) {
+	// for simplicity, update the listing rather than updating single value
+	tx, err := Client.Begin()
+	if err != nil {
+		return u, err
+	}
+
+	// create SQL statement for db update
+	sqlStatement := `INSERT INTO coindrop_auth (auth_user_id) VALUES ($1)`
+
+	// prepare statement
+	stmt, err := Client.Prepare(sqlStatement)
+	if err != nil {
+		return u, err
+	}
+
+	defer stmt.Close()
+
+	// execute db write using unique ID as the identifier
+	_, err = stmt.Exec(u.Info.AuthUserID)
+	if err != nil {
+		// rollback transaction if error thrown
+		tx.Rollback()
+		return u, err
+	}
+
+	// commit db write
+	err = tx.Commit()
+	if err != nil {
+		// rollback transaction if error thrown
+		tx.Rollback()
+		return u, err
+	}
+
+	return u, nil
+}
+
+// WALLET
+
 // UpdateWallet updates the wallet address of a single user
 func UpdateWallet(u *User) (*User, error) {
 	// for simplicity, update the listing rather than updating single value
@@ -34,6 +74,34 @@ func UpdateWallet(u *User) (*User, error) {
 	if err != nil {
 		// rollback transaction if error thrown
 		tx.Rollback()
+		return u, err
+	}
+
+	return u, nil
+}
+
+// GetWallet updates the wallet address of a single user
+func GetWallet(u *User) (*User, error) {
+	// create SQL statement for db update
+	sqlStatement := `SELECT wallet_address FROM coindrop_auth WHERE auth_user_id=$1`
+
+	// prepare statement
+	stmt, err := Client.Prepare(sqlStatement)
+	if err != nil {
+		return u, err
+	}
+
+	defer stmt.Close()
+
+	// initialize row object
+	row := stmt.QueryRow(u.Info.AuthUserID)
+
+	// iterate over row object to retrieve queried value
+	err = row.Scan(
+		&u.Info.WalletAddress,
+	)
+
+	if err != nil {
 		return u, err
 	}
 
