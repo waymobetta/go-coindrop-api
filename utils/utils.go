2958efdf9ef71153e9bd3ef8191d2bcddc9944a7
waymobetta <jbroethke@gmail.com>
2019-01-21 22:44:51 -0800

    add comments

--- utils/utils.go
+++ utils/utils.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 )
 
+// Message handles setting universal format of messaging for API responses
 func Message(status bool, message interface{}) map[string]interface{} {
 	return map[string]interface{}{"status": status, "message": message}
 }
 
+// Respond handles setting universal format of structure for API responses
 func Respond(w http.ResponseWriter, data map[string]interface{}) {
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
