8480c179e1edb998c46a9954e1ed7b14bbdbfb00
waymobetta <jbroethke@gmail.com>
2019-01-27 15:14:05 -0800

    add methods to manage user preferences

---
+++ db/preferences.go
@@ -0,0 +1,11 @@
+package db
+
+// GetUserPreferences method returns all user's preferences
+func GetUserPreferences() {
+
+}
+
+// UpdateUserPreferences method updates user's preferences
+func UpdateUserPreferences() {
+
+}
---
+++ handlers/preferences.go
@@ -0,0 +1,11 @@
+package handlers
+
+// PreferencesGet method returns user's preferences
+func PreferencesGet() {
+
+}
+
+// PreferencesUpdate method updates user's preferences
+func PreferencesUpdate() {
+
+}
