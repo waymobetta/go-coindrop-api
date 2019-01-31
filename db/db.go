97289bb4abf28edbc5cd13faf500d14bf0c11f2b
waymobetta <jbroethke@gmail.com>
2019-01-31 11:52:42 -0800

    update environment variables + add API environment configurations

--- db/db.go
+++ db/db.go
@@ -11,15 +11,28 @@ import (
 
 // New initializes a new db connection; synonymous with init
 func New() error {
+	// clear screen
 	wmb.Clear()
 
 	args := os.Args
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbname)
+	if len(args) < 2 {
+		fmt.Println("usage: go-coindrop-api <localhost/staging/prod>")
+		os.Exit(0)
+	}
 
-	if len(args) > 1 {
-		fmt.Println("[+] Connecting to localhost\n")
+	var psqlInfo string
+
+	switch {
+	case args[1] == "localhost":
+		fmt.Println("[*] Connecting to localhost")
 		psqlInfo = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", lHost, lPort, lUser, lPassword, lDbname, lSslmode)
+	case args[1] == "staging":
+		fmt.Println("[*] Connecting to staging")
+		psqlInfo = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbname)
+	case args[1] == "prod":
+		fmt.Println("[*] Connecting to prod")
+		psqlInfo = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", pHost, pPort, pUser, pPassword, pDbname)
 	}
 
 	_client, err := sql.Open("postgres", psqlInfo)
@@ -27,6 +40,6 @@ func New() error {
 	if err != nil {
 		return err
 	}
-	fmt.Println("api ready..\n")
+	fmt.Println("api ready..")
 	return nil
 }
--- db/variables.go
+++ db/variables.go
@@ -13,12 +13,19 @@ var (
 	// Client is a pointer to the sql DB object
 	Client *sql.DB
 
-	// environment variables for db
+	// staging
 	host     = os.Getenv("AWS_COINDROP_STAGING_HOST")
 	port, _  = strconv.Atoi(os.Getenv("AWS_COINDROP_STAGING_PORT"))
 	user     = os.Getenv("AWS_COINDROP_STAGING_USER")
 	dbname   = os.Getenv("AWS_COINDROP_STAGING_DBNAME")
 	password = os.Getenv("AWS_COINDROP_STAGING_PASS")
+
+	// production
+	pHost     = os.Getenv("AWS_COINDROP_PROD_HOST")
+	pPort, _  = strconv.Atoi(os.Getenv("AWS_COINDROP_PROD_PORT"))
+	pUser     = os.Getenv("AWS_COINDROP_PROD_USER")
+	pDbname   = os.Getenv("AWS_COINDROP_PROD_DBNAME")
+	pPassword = os.Getenv("AWS_COINDROP_PROD_PASS")
 
 	// environment variables for local db
 	lHost     = os.Getenv("LOCAL_PG_HOST")
