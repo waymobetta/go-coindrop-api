dcfb27918ae0a648be62dd7e98f940591875bdaa
waymobetta <jbroethke@gmail.com>
2019-02-09 21:37:29 -0800

    add quiz controller

---
+++ controllers/quiz.go
@@ -0,0 +1,49 @@
+package controllers
+
+import (
+	"github.com/goadesign/goa"
+	log "github.com/sirupsen/logrus"
+	"github.com/waymobetta/go-coindrop-api/app"
+	"github.com/waymobetta/go-coindrop-api/db"
+)
+
+// QuizController implements the quiz resource.
+type QuizController struct {
+	*goa.Controller
+	db *db.DB
+}
+
+// NewQuizController creates a quiz controller.
+func NewQuizController(service *goa.Service, db *db.DB) *QuizController {
+	return &QuizController{
+		Controller: service.NewController("QuizController"),
+		db:         db,
+	}
+}
+
+// Show runs the show action.
+func (c *QuizController) Show(ctx *app.ShowQuizContext) error {
+	// QuizController_Show: start_implement
+
+	// Put your logic here
+
+	quiz := new(db.Quiz)
+	quiz.Title = ctx.Params.Get("quizTitle")
+
+	_, err := c.db.GetQuiz(quiz)
+	if err != nil {
+		log.Errorf("[controller/quiz] %v", err)
+		return ctx.NotFound(&app.StandardError{
+			Code:    400,
+			Message: "could not get quiz info from db",
+		})
+	}
+
+	log.Printf("[controller/quiz] returned information for coindrop quiz: %v\n", quiz.Title)
+
+	res := &app.Quiz{
+		Quiz: quiz,
+	}
+	return ctx.OK(res)
+	// QuizController_Show: end_implement
+}
