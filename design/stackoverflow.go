7e5f49ebe4d733db9287fdedca969e69fa903060
waymobetta <jbroethke@gmail.com>
2019-03-08 13:30:49 -0800

    [design] update stack overflow create user payload property type

--- design/stackoverflow.go
+++ design/stackoverflow.go
@@ -67,7 +67,7 @@ var CreateStackOverflowUserPayload = Type("CreateStackOverflowUserPayload", func
 		Pattern("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$")
 		Example("9302608f-f6a4-4004-b088-63e5fb43cc26")
 	})
-	Attribute("stackUserId", String, "Stack Overflow User ID")
+	Attribute("stackUserId", Integer, "Stack Overflow Community-Specific Account ID")
 	Required(
 		"userId",
 		"stackUserId",
