abfe15b77311cd23178858ce6f27edc5ca6168f7
waymobetta <jbroethke@gmail.com>
2019-03-08 13:27:43 -0800

    [services/stackoverflow] fix logic + property names

--- services/stackoverflow/stackoverflow.go
+++ services/stackoverflow/stackoverflow.go
@@ -9,8 +9,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
-	"github.com/waymobetta/go-coindrop-api/db"
-	"github.com/waymobetta/go-coindrop-api/verify"
+	"github.com/waymobetta/go-coindrop-api/types"
 )
 
 const (
@@ -18,19 +17,11 @@ const (
 	baseAPI = "https://api.stackexchange.com/2.2"
 )
 
-var (
-	// TODO:
-	// remove global client
-	client = &http.Client{
-		Timeout: time.Duration(time.Second * 10),
-	}
-)
-
 // GetProfileByUserID fetches basic user profile info by unique user ID
-func GetProfileByUserID(u *db.User) (*db.User, error) {
-	log.Printf("[stackoverflow] collecting profile information for user ID: %v\n", u.StackOverflow.UserID)
+func GetProfileByUserID(u *types.User) (*types.User, error) {
+	log.Printf("[stackoverflow] collecting profile information for user ID: %v\n", u.Social.StackOverflow.StackUserID)
 
-	profileEndpoint := fmt.Sprintf("/users/%v?order=desc&sort=reputation&site=stackoverflow&filter=!-*jbN*IioeFP", u.StackOverflow.UserID)
+	profileEndpoint := fmt.Sprintf("/users/%v?order=desc&sort=reputation&site=stackoverflow&filter=!-*jbN*IioeFP", u.Social.StackOverflow.StackUserID)
 
 	// concatenate endpoint with base URL of db
 	profileURL := fmt.Sprintf("%s%s", baseAPI, profileEndpoint)
@@ -38,15 +29,19 @@ func GetProfileByUserID(u *db.User) (*db.User, error) {
 	// prepare GET request
 	req, err := http.NewRequest("GET", profileURL, nil)
 	if err != nil {
-		log.Errorf("[stackoverflow] Error preparing GET request for user profile info; %v\n", err)
+		log.Errorf("[services/stackoverflow] Error preparing GET request for user profile info; %v\n", err)
 		return u, err
 	}
 	req.Header.Set("Content-Type", "application/json")
 
+	client := &http.Client{
+		Timeout: time.Duration(time.Second * 10),
+	}
+
 	// execute GET request
 	res, err := client.Do(req)
 	if err != nil {
-		log.Errorf("[stackoverflow] Error fetching user profile info; %v\n", err)
+		log.Errorf("[services/stackoverflow] Error fetching user profile info; %v\n", err)
 		return u, err
 	}
 	defer res.Body.Close()
@@ -54,7 +49,7 @@ func GetProfileByUserID(u *db.User) (*db.User, error) {
 	// read result of GET request
 	byteArr, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Errorf("[stackoverflow] Error reading response body; %v\n", err)
+		log.Errorf("[services/stackoverflow] Error reading response body; %v\n", err)
 		return u, err
 	}
 
@@ -63,11 +58,11 @@ func GetProfileByUserID(u *db.User) (*db.User, error) {
 
 	// unmarshal JSON into AboutProfileResponse struct
 	if err := json.Unmarshal(byteArr, &aboutProfResStruct); err != nil {
-		log.Errorf("[stackoverflow] Error unmarshalling JSON; %v\n", err)
+		log.Errorf("[services/stackoverflow] Error unmarshalling JSON; %v\n", err)
 		return u, err
 	}
 
-	log.Printf("[stackoverflow] found profile info for user: %s!\n", aboutProfResStruct.Items[0].DisplayName)
+	log.Printf("[services/stackoverflow] found profile info for user: %s!\n", aboutProfResStruct.Items[0].DisplayName)
 
 	// initialize empty accounts slice
 	accounts := []string{}
@@ -75,28 +70,32 @@ func GetProfileByUserID(u *db.User) (*db.User, error) {
 	// iterate over number of items in the response
 	// NOTE: there should only be a single item
 	for index := range aboutProfResStruct.Items {
-		u.StackOverflow = db.StackOverflow{
-			DisplayName:       aboutProfResStruct.Items[0].DisplayName,
-			ExchangeAccountID: aboutProfResStruct.Items[index].AccountID,
-			UserID:            aboutProfResStruct.Items[index].UserID,
-			Accounts:          accounts,
-			Communities: []db.Community{
-				db.Community{
-					Name:          "stackoverflow",
-					Reputation:    aboutProfResStruct.Items[index].Reputation,
-					QuestionCount: aboutProfResStruct.Items[index].QuestionCount,
-					AnswerCount:   aboutProfResStruct.Items[index].AnswerCount,
-					BadgeCounts: map[string]int{
-						"Bronze": aboutProfResStruct.Items[index].BadgeCounts.Bronze,
-						"Silver": aboutProfResStruct.Items[index].BadgeCounts.Silver,
-						"Gold":   aboutProfResStruct.Items[index].BadgeCounts.Gold,
+		u = &types.User{
+			Social: &types.Social{
+				StackOverflow: &types.StackOverflow{
+					DisplayName:       aboutProfResStruct.Items[0].DisplayName,
+					ExchangeAccountID: aboutProfResStruct.Items[index].AccountID,
+					StackUserID:       aboutProfResStruct.Items[index].UserID,
+					Accounts:          accounts,
+					Communities: []types.Community{
+						types.Community{
+							Name:          "stackoverflow",
+							Reputation:    aboutProfResStruct.Items[index].Reputation,
+							QuestionCount: aboutProfResStruct.Items[index].QuestionCount,
+							AnswerCount:   aboutProfResStruct.Items[index].AnswerCount,
+							BadgeCounts: map[string]int{
+								"Bronze": aboutProfResStruct.Items[index].BadgeCounts.Bronze,
+								"Silver": aboutProfResStruct.Items[index].BadgeCounts.Silver,
+								"Gold":   aboutProfResStruct.Items[index].BadgeCounts.Gold,
+							},
+						},
+					},
+					Verification: &types.Verification{
+						PostedVerificationCode:    aboutProfResStruct.Items[index].AboutMe,
+						ConfirmedVerificationCode: u.Social.StackOverflow.Verification.ConfirmedVerificationCode,
+						Verified:                  false,
 					},
 				},
-			},
-			Verification: &verify.Verification2{
-				PostedVerificationCode:    aboutProfResStruct.Items[index].AboutMe,
-				ConfirmedVerificationCode: u.StackOverflow.Verification.ConfirmedVerificationCode,
-				Verified:                  false,
 			},
 		}
 		return u, nil
@@ -105,10 +104,10 @@ func GetProfileByUserID(u *db.User) (*db.User, error) {
 }
 
 // GetAssociatedAccounts method fetches associated communities of user
-func GetAssociatedAccounts(u *db.User) (*db.User, error) {
-	log.Printf("[stackoverflow] collecting associated account information for user: %s\n", u.StackOverflow.DisplayName)
+func GetAssociatedAccounts(u *types.User) (*types.User, error) {
+	log.Printf("[services/stackoverflow] collecting associated account information for user: %s\n", u.Social.StackOverflow.DisplayName)
 
-	associatedAccountsEndpoint := fmt.Sprintf("/users/%v/associated", u.StackOverflow.ExchangeAccountID)
+	associatedAccountsEndpoint := fmt.Sprintf("/users/%v/associated", u.Social.StackOverflow.ExchangeAccountID)
 
 	// concatenate endpoint with base URL of db
 	associatedAccountsURL := fmt.Sprintf("%s%s", baseAPI, associatedAccountsEndpoint)
@@ -116,15 +115,19 @@ func GetAssociatedAccounts(u *db.User) (*db.User, error) {
 	// prepare GET request
 	req, err := http.NewRequest("GET", associatedAccountsURL, nil)
 	if err != nil {
-		log.Errorf("[stackoverflow] Error preparing GET request for user associated accounts info; %v\n", err)
+		log.Errorf("[services/stackoverflow] Error preparing GET request for user associated accounts info; %v\n", err)
 		return u, err
 	}
 	req.Header.Set("Content-Type", "application/json")
 
+	client := &http.Client{
+		Timeout: time.Duration(time.Second * 10),
+	}
+
 	// execute GET request
 	res, err := client.Do(req)
 	if err != nil {
-		log.Errorf("[stackoverflow] Error fetching user profile info; %v\n", err)
+		log.Errorf("[services/stackoverflow] Error fetching user profile info; %v\n", err)
 		return u, err
 	}
 	defer res.Body.Close()
@@ -137,7 +140,7 @@ func GetAssociatedAccounts(u *db.User) (*db.User, error) {
 	// read result of GET request
 	byteArr, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Errorf("[stackoverflow] Error reading response body\n%v", err)
+		log.Errorf("[services/stackoverflow] Error reading response body\n%v", err)
 		return u, err
 	}
 
@@ -146,20 +149,20 @@ func GetAssociatedAccounts(u *db.User) (*db.User, error) {
 
 	// unmarshal JSON into AssociatedCommunitiesResponse struct
 	if err := json.Unmarshal(byteArr, &associatedCommunitiesStruct); err != nil {
-		log.Errorf("[stackoverflow] Error unmarshalling JSON; %v\n", err)
+		log.Errorf("[services/stackoverflow] Error unmarshalling JSON; %v\n", err)
 		return u, err
 	}
 
-	log.Printf("[stackoverflow] Found associated account info for user: %s!\n", u.StackOverflow.DisplayName)
+	log.Printf("[services/stackoverflow] Found associated account info for user: %s!\n", u.Social.StackOverflow.DisplayName)
 
 	// initialize new struct object to hold Community data
-	communityObj := db.Community{}
+	communityObj := types.Community{}
 
 	// iterate over number of items in the response
 	// NOTE: there could be multiple items
 	for index := range associatedCommunitiesStruct.Items {
 		// for each item, overwrite struct object to hold updated data
-		communityObj = db.Community{
+		communityObj = types.Community{
 			Name:          associatedCommunitiesStruct.Items[index].SiteName,
 			Reputation:    associatedCommunitiesStruct.Items[index].Reputation,
 			QuestionCount: associatedCommunitiesStruct.Items[index].QuestionCount,
@@ -172,10 +175,10 @@ func GetAssociatedAccounts(u *db.User) (*db.User, error) {
 		}
 
 		// append community name to account slice
-		u.StackOverflow.Accounts = append(u.StackOverflow.Accounts, associatedCommunitiesStruct.Items[index].SiteName)
+		u.Social.StackOverflow.Accounts = append(u.Social.StackOverflow.Accounts, associatedCommunitiesStruct.Items[index].SiteName)
 
 		// append updated struct data to slice of objects
-		u.StackOverflow.Communities = append(u.StackOverflow.Communities, communityObj)
+		u.Social.StackOverflow.Communities = append(u.Social.StackOverflow.Communities, communityObj)
 	}
 
 	return u, nil
