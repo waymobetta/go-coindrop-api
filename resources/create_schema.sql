965e22286f42e146ed886deb0fc7061dbf9bf2cb
waymobetta <jbroethke@gmail.com>
2019-03-19 10:25:35 -0700

    [schema] add updated_at column to all tables

--- resources/create_schema.sql
+++ resources/create_schema.sql
@@ -15,7 +15,8 @@ LANGUAGE plpgsql;
 CREATE TABLE IF NOT EXISTS coindrop_auth (
 	id uuid DEFAULT uuid_generate_v4() UNIQUE,
 	cognito_auth_user_id TEXT NOT NULL UNIQUE,
-	created_at timestamp with time zone DEFAULT now()
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone
 );
 
 -- WALLETS
@@ -24,7 +25,8 @@ CREATE TABLE coindrop_wallets (
     address text,
     user_id uuid REFERENCES coindrop_auth(id),
     type text,
-    created_at timestamp with time zone DEFAULT now()
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone
 );
 
 -- REDDIT
@@ -39,7 +41,8 @@ CREATE TABLE IF NOT EXISTS coindrop_reddit (
 	posted_verification_code TEXT NOT NULL,
 	confirmed_verification_code TEXT DEFAULT gen_verif_code() UNIQUE,
 	verified BOOLEAN NOT NULL,
-	created_at timestamp with time zone DEFAULT now()
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone
 );
 
 -- STACK OVERFLOW
@@ -53,7 +56,8 @@ CREATE TABLE IF NOT EXISTS coindrop_stackoverflow (
 	posted_verification_code TEXT NOT NULL,
 	confirmed_verification_code TEXT DEFAULT gen_verif_code() UNIQUE,
 	verified BOOLEAN NOT NULL,
-	created_at timestamp with time zone DEFAULT now()
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone
 );
 
 -- BADGE
@@ -61,7 +65,8 @@ CREATE TABLE IF NOT EXISTS coindrop_badges (
 	id uuid DEFAULT uuid_generate_v4() UNIQUE,
 	name TEXT NOT NULL,
 	description TEXT,
-	created_at timestamp with time zone DEFAULT now()
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone
 );
 
 -- TASKS
@@ -75,7 +80,8 @@ CREATE TABLE IF NOT EXISTS coindrop_tasks (
     token_allocation integer,
     badge_id uuid REFERENCES coindrop_badges(id),
     logo_url text,
-    created_at timestamp with time zone DEFAULT now()
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone
 );
 
 -- QUIZZES
@@ -84,7 +90,8 @@ CREATE TABLE IF NOT EXISTS coindrop_quizzes (
 	title TEXT NOT NULL,
 	typeform_form_url TEXT NOT NULL,
 	typeform_form_id TEXT NOT NULL,
-	created_at timestamp with time zone DEFAULT now()
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone
 );
 
 -- QUIZ RESULTS
@@ -95,7 +102,8 @@ CREATE TABLE IF NOT EXISTS coindrop_quiz_results (
 	questions_correct INTEGER NOT NULL,
 	questions_incorrect INTEGER NOT NULL,
 	quiz_taken BOOLEAN,
-	created_at timestamp with time zone DEFAULT now()
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone
 );
 
 -- TASKS SPECIFIC TO USER
@@ -104,7 +112,8 @@ CREATE TABLE IF NOT EXISTS coindrop_user_tasks (
 	user_id uuid REFERENCES coindrop_auth(id),
 	task_id uuid REFERENCES coindrop_tasks(id),
 	completed BOOLEAN DEFAULT false,
-	created_at timestamp with time zone DEFAULT now()
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone
 );
 
 CREATE UNIQUE INDEX coindrop_wallets_user_id_type_uniq_idx ON coindrop_wallets(user_id uuid_ops,type text_ops);
@@ -115,14 +124,16 @@ CREATE UNIQUE INDEX coindrop_wallets_user_id_type_uniq_idx ON coindrop_wallets(u
 CREATE TABLE IF NOT EXISTS coindrop_reddit_communities (
 	id uuid DEFAULT uuid_generate_v4() UNIQUE,
 	name TEXT NOT NULL,
-	created_at timestamp with time zone DEFAULT now()
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone
 );
 
 -- STACK OVERFLOW COMMUNITIES
 CREATE TABLE IF NOT EXISTS coindrop_stackoverflow_communities (
 	id uuid DEFAULT uuid_generate_v4() UNIQUE,
 	name TEXT NOT NULL,
-	created_at timestamp with time zone DEFAULT now()
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone
 );
 
 CREATE TABLE "public"."coindrop_profiles" (
@@ -132,5 +143,6 @@ CREATE TABLE "public"."coindrop_profiles" (
     "username" text,
     "image_url" text,
     PRIMARY KEY ("id"),
-    created_at timestamp with time zone DEFAULT now()
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone
 );
