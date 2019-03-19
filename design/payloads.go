004220a730576db1e192befadb580d239e29944c
waymobetta <jbroethke@gmail.com>
2019-03-19 16:32:30 -0700

    [design] rename type to walletType

--- design/payloads.go
+++ design/payloads.go
@@ -35,7 +35,10 @@ var WalletPayload = Type("WalletPayload", func() {
 		Example("0x845fdD93Cca3aE9e380d5556818e6d0b902B977c")
 	})
 	Attribute("walletType", String, "wallet type")
-	Required("walletAddress", "type")
+	Required(
+		"walletAddress",
+		"walletType",
+	)
 })
 
 // CreateUserPayload is the payload for creating a listing for a user's reddit info
