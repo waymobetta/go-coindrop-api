2cd596205f4c205d2e967aaa3957e156cf5e8b02
waymobetta <jbroethke@gmail.com>
2019-03-19 09:59:22 -0700

    [schema] add created_at column to all tables

--- resources/create_schema.sql
+++ resources/create_schema.sql
@@ -14,7 +14,8 @@ LANGUAGE plpgsql;
 -- AUTH
 CREATE TABLE IF NOT EXISTS coindrop_auth (
 	id uuid DEFAULT uuid_generate_v4() UNIQUE,
-	cognito_auth_user_id TEXT NOT NULL UNIQUE
+	cognito_auth_user_id TEXT NOT NULL UNIQUE,
+	created_at timestamp with time zone DEFAULT now()
 );
 
 -- WALLETS
@@ -22,7 +23,8 @@ CREATE TABLE coindrop_wallets (
     id uuid DEFAULT uuid_generate_v4() UNIQUE,
     address text,
     user_id uuid REFERENCES coindrop_auth(id),
-    type text
+    type text,
+    created_at timestamp with time zone DEFAULT now()
 );
 
 -- REDDIT
@@ -36,7 +38,8 @@ CREATE TABLE IF NOT EXISTS coindrop_reddit (
 	trophies TEXT ARRAY NOT NULL,
 	posted_verification_code TEXT NOT NULL,
 	confirmed_verification_code TEXT DEFAULT gen_verif_code() UNIQUE,
-	verified BOOLEAN NOT NULL
+	verified BOOLEAN NOT NULL,
+	created_at timestamp with time zone DEFAULT now()
 );
 
 -- STACK OVERFLOW
@@ -49,14 +52,16 @@ CREATE TABLE IF NOT EXISTS coindrop_stackoverflow (
 	accounts TEXT ARRAY NOT NULL,
 	posted_verification_code TEXT NOT NULL,
 	confirmed_verification_code TEXT DEFAULT gen_verif_code() UNIQUE,
-	verified BOOLEAN NOT NULL
+	verified BOOLEAN NOT NULL,
+	created_at timestamp with time zone DEFAULT now()
 );
 
 -- BADGE
 CREATE TABLE IF NOT EXISTS coindrop_badges (
 	id uuid DEFAULT uuid_generate_v4() UNIQUE,
 	name TEXT NOT NULL,
-	description TEXT
+	description TEXT,
+	created_at timestamp with time zone DEFAULT now()
 );
 
 -- TASKS
@@ -69,7 +74,8 @@ CREATE TABLE IF NOT EXISTS coindrop_tasks (
     token_name text,
     token_allocation integer,
     badge_id uuid REFERENCES coindrop_badges(id),
-    logo_url text
+    logo_url text,
+    created_at timestamp with time zone DEFAULT now()
 );
 
 -- QUIZZES
@@ -77,7 +83,8 @@ CREATE TABLE IF NOT EXISTS coindrop_quizzes (
 	id uuid DEFAULT uuid_generate_v4() UNIQUE,
 	title TEXT NOT NULL,
 	typeform_form_url TEXT NOT NULL,
-	typeform_form_id TEXT NOT NULL
+	typeform_form_id TEXT NOT NULL,
+	created_at timestamp with time zone DEFAULT now()
 );
 
 -- QUIZ RESULTS
@@ -87,7 +94,8 @@ CREATE TABLE IF NOT EXISTS coindrop_quiz_results (
 	user_id uuid REFERENCES coindrop_auth(id),
 	questions_correct INTEGER NOT NULL,
 	questions_incorrect INTEGER NOT NULL,
-	quiz_taken BOOLEAN
+	quiz_taken BOOLEAN,
+	created_at timestamp with time zone DEFAULT now()
 );
 
 -- TASKS SPECIFIC TO USER
@@ -95,7 +103,8 @@ CREATE TABLE IF NOT EXISTS coindrop_user_tasks (
 	id uuid DEFAULT uuid_generate_v4() UNIQUE,
 	user_id uuid REFERENCES coindrop_auth(id),
 	task_id uuid REFERENCES coindrop_tasks(id),
-	completed BOOLEAN DEFAULT false
+	completed BOOLEAN DEFAULT false,
+	created_at timestamp with time zone DEFAULT now()
 );
 
 CREATE UNIQUE INDEX coindrop_wallets_user_id_type_uniq_idx ON coindrop_wallets(user_id uuid_ops,type text_ops);
@@ -105,13 +114,15 @@ CREATE UNIQUE INDEX coindrop_wallets_user_id_type_uniq_idx ON coindrop_wallets(u
 -- REDDIT COMMUNITIES
 CREATE TABLE IF NOT EXISTS coindrop_reddit_communities (
 	id uuid DEFAULT uuid_generate_v4() UNIQUE,
-	name TEXT NOT NULL
+	name TEXT NOT NULL,
+	created_at timestamp with time zone DEFAULT now()
 );
 
 -- STACK OVERFLOW COMMUNITIES
 CREATE TABLE IF NOT EXISTS coindrop_stackoverflow_communities (
 	id uuid DEFAULT uuid_generate_v4() UNIQUE,
-	name TEXT NOT NULL
+	name TEXT NOT NULL,
+	created_at timestamp with time zone DEFAULT now()
 );
 
 CREATE TABLE "public"."coindrop_profiles" (
@@ -120,5 +131,6 @@ CREATE TABLE "public"."coindrop_profiles" (
     "name" text,
     "username" text,
     "image_url" text,
-    PRIMARY KEY ("id")
+    PRIMARY KEY ("id"),
+    created_at timestamp with time zone DEFAULT now()
 );
