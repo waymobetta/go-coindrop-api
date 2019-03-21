33c474986a20f31234a7c6566a704bf37b49d66e
waymobetta <jbroethke@gmail.com>
2019-03-21 15:11:46 -0700

    [design/payloads] add userID field

--- design/payloads.go
+++ design/payloads.go
@@ -30,12 +30,17 @@ var TaskPayload = Type("TaskPayload", func() {
 // WalletPayload is the payload for updating a user's wallet
 var WalletPayload = Type("WalletPayload", func() {
 	Description("Wallet payload")
+	Attribute("userId", String, "User ID", func() {
+		Pattern("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$")
+		Example("9302608f-f6a4-4004-b088-63e5fb43cc26")
+	})
 	Attribute("walletAddress", String, "Wallet address", func() {
 		Pattern("^0x[0-9a-fA-F]{40}$")
 		Example("0x845fdD93Cca3aE9e380d5556818e6d0b902B977c")
 	})
 	Attribute("walletType", String, "wallet type")
 	Required(
+		"userId",
 		"walletAddress",
 		"walletType",
 	)
