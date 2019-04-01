04bebb8ef78e96ff8faec4105b4e35b094961daa
waymobetta <jbroethke@gmail.com>
2019-04-01 14:58:45 -0700

    [services/ethereum] add ethereum package

---
+++ services/ethereum/ethereum.go
@@ -0,0 +1,162 @@
+package ethereum
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"log"
+	"math/big"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"golang.org/x/crypto/sha3"
+)
+
+const (
+	PRIVATE_KEY            = ""
+	TOKEN_CONTRACT_ADDRESS = ""
+)
+
+// SendEther ...
+func SendEther(recipientAddress string, ethAmountInWei int64) (string, error) {
+	client, err := ethclient.Dial("https://rinkeby.infura.io")
+	if err != nil {
+		return "", err
+	}
+
+	privateKey, err := crypto.HexToECDSA(PRIVATE_KEY)
+	if err != nil {
+		return "", err
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return "", err
+	}
+
+	value := big.NewInt(ethAmountInWei) // in wei
+	gasLimit := uint64(21000)           // in units
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return "", err
+	}
+
+	toAddress := common.HexToAddress(recipientAddress)
+	var data []byte
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		return "", err
+	}
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		return "", err
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		return "", err
+	}
+
+	transaction := signedTx.Hash().Hex()
+
+	log.Printf("https://rinkeby.etherscan.io/tx/%s\n", transaction)
+
+	return transaction, nil
+}
+
+// SendToken ...
+func SendToken(tokenAmount, recipientAddress string) (string, error) {
+	client, err := ethclient.Dial("https://rinkeby.infura.io")
+	if err != nil {
+		return "", err
+	}
+
+	privateKey, err := crypto.HexToECDSA(PRIVATE_KEY)
+	if err != nil {
+		return "", err
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return "", err
+	}
+
+	value := big.NewInt(0) // in wei (0 eth)
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return "", err
+	}
+
+	toAddress := common.HexToAddress(recipientAddress)
+	tokenAddress := common.HexToAddress(TOKEN_CONTRACT_ADDRESS)
+
+	transferFnSignature := []byte("transfer(address,uint256)")
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write(transferFnSignature)
+	methodID := hash.Sum(nil)[:4]
+
+	paddedAddress := common.LeftPadBytes(toAddress.Bytes(), 32)
+
+	amount := new(big.Int)
+	amount.SetString(tokenAmount, 10) // sets the value to 1000 tokens, in the token denomination
+
+	paddedAmount := common.LeftPadBytes(amount.Bytes(), 32)
+
+	var data []byte
+	data = append(data, methodID...)
+	data = append(data, paddedAddress...)
+	data = append(data, paddedAmount...)
+
+	gasLimit, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
+		To:   &tokenAddress,
+		Data: data,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	// autoset gas for testing
+	gasLimit = 200000
+	gasPrice = big.NewInt(41000000000)
+
+	tx := types.NewTransaction(nonce, tokenAddress, value, gasLimit, gasPrice, data)
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		return "", err
+	}
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		return "", err
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		return "", err
+	}
+
+	transaction := signedTx.Hash().Hex()
+
+	log.Printf("https://rinkeby.etherscan.io/tx/%s\n", transaction)
+	return transaction, nil
+}
