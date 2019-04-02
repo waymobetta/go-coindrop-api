2e11be6e412e99fa104210b46eb2f25b78526d08
waymobetta <jbroethke@gmail.com>
2019-04-02 12:57:32 -0700

    [schema] add transaction table

--- resources/drop_schema.sql
+++ resources/drop_schema.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS coindrop_reddit;
 DROP TABLE IF EXISTS coindrop_stackoverflow;
 DROP TABLE IF EXISTS coindrop_quiz_results;
 DROP TABLE IF EXISTS coindrop_quizzes;
+DROP TABLE IF EXISTS coindrop_transactions;
 DROP TABLE IF EXISTS coindrop_user_tasks;
 DROP TABLE IF EXISTS coindrop_tasks;
 DROP TABLE IF EXISTS coindrop_wallets;
