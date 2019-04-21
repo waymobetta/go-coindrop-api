1610ab4f172572d101af042883d372d1440df694
waymobetta <jbroethke@gmail.com>
2019-04-21 12:05:59 -0700

    [db/targeting] add method to get all user IDs + subreddits

--- db/targeting.go
+++ db/targeting.go
@@ -2,6 +2,9 @@ package db
 
 import (
 	"database/sql"
+	"encoding/json"
+
+	"github.com/waymobetta/go-coindrop-api/types"
 )
 
 // Subreddits is a list of approved subreddits used for targeting
@@ -150,6 +153,73 @@ func (db *DB) GetEligibleRedditUsersAcrossMultipleSubs(sub1, sub2 string, thresh
 		}
 		// append user ID to slice of users
 		usersSlice = append(usersSlice, user.String)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return usersSlice, nil
+}
+
+// GetRedditUsersAndSubs returns IDs and subreddits of all reddit users (anonymous)
+func (db *DB) GetRedditUsersAndSubs() ([]types.User, error) {
+	usersSlice := []types.User{}
+
+	// create SQL statement for db query
+	// pulls a list of all user_id who are above or equal to the eligibility threshold of multiple subreddits
+	sqlStatement := `
+		SELECT
+			user_id,
+			subreddits
+		FROM
+			coindrop_reddit
+	`
+
+	// execute db query by passing in prepared SQL statement
+	rows, err := db.client.Query(
+		sqlStatement,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	// iterate over rows
+	for rows.Next() {
+		user := types.User{}
+
+		var userID sql.NullString
+		var accountsString sql.NullString
+		var accountsMap map[string]int
+
+		err = rows.Scan(
+			&user.UserID,
+			&accountsString,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		user.UserID = userID.String
+
+		if accountsString.String == "" {
+			user.Social.Reddit.Subreddits = accountsMap
+		}
+
+		byteArr := []byte(accountsString.String)
+
+		err = json.Unmarshal(byteArr, &accountsMap)
+		if err != nil {
+			return nil, err
+		}
+
+		user.Social.Reddit.Subreddits = accountsMap
+
+		// append user ID to slice of users
+		usersSlice = append(usersSlice, user)
 	}
 
 	err = rows.Err()
