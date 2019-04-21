34e6e2b4e4092e724771d9552fb8a3fb87b8c185
waymobetta <jbroethke@gmail.com>
2019-04-21 10:47:42 -0700

    [design/targeting] add endpoints for targeting

---
+++ design/targeting.go
@@ -0,0 +1,33 @@
+package design
+
+import (
+	. "github.com/goadesign/goa/design"        // Use . imports to enable the DSL
+	. "github.com/goadesign/goa/design/apidsl" // Use . imports to enable the DSL
+)
+
+var _ = Resource("targeting", func() {
+	BasePath("/v1/targeting")
+
+	Security(JWTAuth)
+
+	Response(NotFound, StandardErrorMedia)
+	Response(BadRequest, StandardErrorMedia)
+	Response(Gone, StandardErrorMedia)
+	Response(InternalServerError, StandardErrorMedia)
+
+	Action("display", func() {
+		Description("Get list of eligible users")
+		Routing(GET("/users/:project"))
+		Params(func() {
+			Param("project", String, "Project name")
+		})
+		Response(OK, TargetingMedia)
+	})
+
+	Action("set", func() {
+		Description("Set users as eligible")
+		Routing(POST("/tasks/set"))
+		Payload(SetTargetingPayload)
+		Response(OK)
+	})
+})
