65655763393d0416c94ba684590823b6ccd6fb8d
waymobetta <jbroethke@gmail.com>
2019-04-22 13:48:12 -0700

    [db/public] fix SQL statement to return all info for badge

--- db/public.go
+++ db/public.go
@@ -14,17 +14,29 @@ func (db *DB) GetBadgesByRedditUsername(redditUsername string) ([]*types.Badge, 
 	// create SQL statement for db query
 	sqlStatement := `
 		SELECT
-			badge_id
+			coindrop_badges.name,
+			coindrop_badges.description,
+			coindrop_badges.logo_url
 		FROM
-			coindrop_public
+			coindrop_badges
+		JOIN
+			coindrop_tasks
+		ON
+			coindrop_tasks.badge_id = coindrop_badges.id
+		JOIN
+			coindrop_user_tasks
+		ON
+			coindrop_user_tasks.task_id = coindrop_tasks.id
 		WHERE
-			user_id = (
+			coindrop_user_tasks.completed = true
+		AND
+			coindrop_user_tasks.user_id = (
 				SELECT
-					user_id
+					coindrop_reddit.user_id
 				FROM
 					coindrop_reddit
 				WHERE
-					username = $1
+					coindrop_reddit.username = $1
 			)
 	`
 
@@ -45,23 +57,26 @@ func (db *DB) GetBadgesByRedditUsername(redditUsername string) ([]*types.Badge, 
 		badge := new(types.Badge)
 
 		var (
-			// name        sql.NullString
-			// description sql.NullString
-			// logoURL     sql.NullString
-			badgeId sql.NullString
+			name        sql.NullString
+			description sql.NullString
+			logoURL     sql.NullString
+			// badgeId sql.NullString
 		)
 
 		err = rows.Scan(
-			&badgeId,
+			&name,
+			&description,
+			&logoURL,
+			// &badgeId,
 		)
 		if err != nil {
 			return nil, err
 		}
 
-		// badge.Name = name.String
-		// badge.Description = description.String
-		// badge.LogoURL = logoURL.String
-		badge.ID = badgeId.String
+		badge.Name = name.String
+		badge.Description = description.String
+		badge.LogoURL = logoURL.String
+		// badge.ID = badgeId.String
 
 		// append badge struct to slice of badges
 		badgeSlice = append(badgeSlice, badge)
