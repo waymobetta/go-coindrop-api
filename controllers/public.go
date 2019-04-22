2a46fa41dc7d82a9a11426ef789fa6b004b2b42c
waymobetta <jbroethke@gmail.com>
2019-04-21 23:20:49 -0700

    [controllers/public] add actions + logic for public fetches

---
+++ controllers/public.go
@@ -0,0 +1,61 @@
+package controllers
+
+import (
+	"fmt"
+
+	"github.com/goadesign/goa"
+	log "github.com/sirupsen/logrus"
+	"github.com/waymobetta/go-coindrop-api/app"
+	"github.com/waymobetta/go-coindrop-api/db"
+)
+
+// PublicController implements the public resource.
+type PublicController struct {
+	*goa.Controller
+	db *db.DB
+}
+
+// NewPublicController creates a public controller.
+func NewPublicController(service *goa.Service, dbs *db.DB) *PublicController {
+	return &PublicController{
+		Controller: service.NewController("PublicController"),
+		db:         dbs,
+	}
+}
+
+// Show runs the show action.
+func (c *PublicController) Show(ctx *app.ShowPublicContext) error {
+	// PublicController_Show: start_implement
+
+	// Put your logic here
+
+	// redditUsername := ctx.Params.Get("redditUsername")
+	redditUsername := ctx.RedditUsername
+
+	var badgeCollection app.BadgeCollection
+
+	badges, err := c.db.GetBadgesByRedditUsername(redditUsername)
+	if err != nil {
+		log.Errorf("[controller/public] error: %v", err)
+		return ctx.InternalServerError(&app.StandardError{
+			Code:    500,
+			Message: "could not get badges from the supplied reddit username",
+		})
+	}
+
+	fmt.Println("BADGES: ", badges)
+
+	for _, badge := range badges {
+		badgeCollection = append(badgeCollection, &app.Badge{
+			Name:        badge.Name,
+			Description: badge.Description,
+			LogoURL:     badge.LogoURL,
+		})
+	}
+
+	res := &app.Public{
+		Badges: badgeCollection,
+	}
+	return ctx.OK(res)
+	// PublicController_Show: end_implement
+}
