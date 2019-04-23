a464e8cf181838c15946786ff180e5fef4f99e23
waymobetta <jbroethke@gmail.com>
2019-04-23 16:07:48 -0700

    [schemas] remove test SQL schema

--- resources/create_table_public.sql
+++
@@ -1,6 +0,0 @@
-create table coindrop_public (
-	id uuid DEFAULT uuid_generate_v4() UNIQUE,
-	badge_id uuid REFERENCES coindrop_badges(id),
-	created_at timestamp with time zone DEFAULT now(),
-	updated_at timestamp with time zone
-);
