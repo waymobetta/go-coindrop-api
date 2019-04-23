4da829d7c9d347a88e7675f3af5c1d07b14bab08
waymobetta <jbroethke@gmail.com>
2019-04-23 16:06:40 -0700

    [db/public] fix SQL statement to reflect new schema changes

--- db/public.go
+++ db/public.go
@@ -14,25 +14,24 @@ func (db *DB) GetBadgesByRedditUsername(redditUsername string) ([]*types.PublicB
 	// create SQL statement for db query
 	sqlStatement := `
 		SELECT
-			coindrop_tasks.author,
-			coindrop_user_tasks.erc721_id,
 			coindrop_badges.name,
+			coindrop_tasks.author,
 			coindrop_badges.description,
-			coindrop_badges.logo_url
+			coindrop_badges.logo_url,
+			coindrop_badges.erc721_contract_address,
+			coindrop_erc721s.token_id
 		FROM
-			coindrop_badges
+			coindrop_erc721s
 		JOIN
 			coindrop_tasks
 		ON
-			coindrop_tasks.badge_id = coindrop_badges.id
+			coindrop_tasks.badge_id = coindrop_erc721s.badge_id
 		JOIN
-			coindrop_user_tasks
+			coindrop_badges
 		ON
-			coindrop_user_tasks.task_id = coindrop_tasks.id
+			coindrop_erc721s.badge_id = coindrop_badges.id
 		WHERE
-			coindrop_user_tasks.completed = true
-		AND
-			coindrop_user_tasks.user_id = (
+			coindrop_erc721s.user_id = (
 				SELECT
 					coindrop_reddit.user_id
 				FROM
@@ -61,37 +60,34 @@ func (db *DB) GetBadgesByRedditUsername(redditUsername string) ([]*types.PublicB
 		}
 
 		var (
-			project           sql.NullString
-			name              sql.NullString
-			description       sql.NullString
-			logoURL           sql.NullString
-			erc721Id          sql.NullString
-			erc721TokenId     sql.NullString
-			erc721ContractId  sql.NullString
-			erc721TotalMinted sql.NullInt64
+			badgeName        sql.NullString
+			badgeProject     sql.NullString
+			badgeDescription sql.NullString
+			badgeLogoURL     sql.NullString
+			contractAddress  sql.NullString
+			tokenId          sql.NullString
 		)
 
 		err = rows.Scan(
-			&project,
-			&erc721Id,
-			&name,
-			&description,
-			&logoURL,
+			&badgeName,
+			&badgeProject,
+			&badgeDescription,
+			&badgeLogoURL,
+			&contractAddress,
+			&tokenId,
 		)
 		if err != nil {
 			return nil, err
 		}
 
-		publicBadge.Project = project.String
-		publicBadge.Name = name.String
-		publicBadge.Description = description.String
-		publicBadge.LogoURL = logoURL.String
-		publicBadge.ERC721.ID = erc721Id.String
-		publicBadge.ERC721.TokenID = erc721TokenId.String
-		publicBadge.ERC721.ContractID = erc721ContractId.String
-		publicBadge.ERC721.TotalMinted = int(erc721TotalMinted.Int64)
+		publicBadge.Name = badgeName.String
+		publicBadge.Project = badgeProject.String
+		publicBadge.Description = badgeDescription.String
+		publicBadge.LogoURL = badgeLogoURL.String
+		publicBadge.ERC721.ContractAddress = contractAddress.String
+		publicBadge.ERC721.TokenID = tokenId.String
 
-		// append badge struct to slice of badges
+		// append publicBadge object to slice of publicBadgeSlice
 		publicBadgeSlice = append(publicBadgeSlice, publicBadge)
 	}
 
