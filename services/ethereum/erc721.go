27af3138cc09ccd2cc733e2a91c30e08f0966a28
waymobetta <jbroethke@gmail.com>
2019-04-24 16:01:16 -0700

    [services/ethereum/erc721] add switch statement to map badge IDs to badge names

--- services/ethereum/erc721.go
+++ services/ethereum/erc721.go
@@ -6,25 +6,26 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-var (
-	badgesSlice = []string{
-		"archaeologist",
-		"colonizer",
-		"associate",
-	}
-)
-
 func generateRandomId() uuid.UUID {
 	uuidString := uuid.NewV4()
 	return uuidString
 }
 
-func MintToken(badgeName string) string {
-	for _, badge := range badgesSlice {
-		if badge == badgeName {
-			uuidString := generateRandomId()
-			return fmt.Sprintf("%s-%s", badge, uuidString)
-		}
+func MintToken(badgeId string) string {
+
+	var badgePrefix string
+
+	switch {
+	case badgeId == "fb3ed1d2-f59c-4dad-a9b3-5135769da144":
+		badgePrefix = "Crypto-conscious"
+	case badgeId == "f2d98e49-dc6c-4594-b6ae-049bea5921d4":
+		badgePrefix = "Archaeologist"
+	case badgeId == "c9e3ba3c-8443-42a8-aa1a-216ac5b5afdb":
+		badgePrefix = "Associate"
+	case badgeId == "9d3d1e37-1afd-43fe-9f38-c90b425e05d5":
+		badgePrefix = "Colonizer"
 	}
-	return fmt.Sprintf("%v", uuid.UUID{})
+
+	uuidString := generateRandomId()
+	return fmt.Sprintf("%s-%s", badgePrefix, uuidString)
 }
