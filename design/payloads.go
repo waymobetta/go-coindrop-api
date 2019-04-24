38e0a4f46e51737d96b38332fc0189459b03be7f
waymobetta <jbroethke@gmail.com>
2019-04-24 15:39:01 -0700

    [design/payloads] add payload for assigning ERC721 tokens

--- design/payloads.go
+++ design/payloads.go
@@ -53,6 +53,25 @@ var SetTargetingPayload = Type("SetTargetingPayload", func() {
 	)
 })
 
+// AssignERC721Payload is the payload for assigning an ERC721 to a user
+var AssignERC721Payload = Type("AssignERC721Payload", func() {
+	Description("AssignERC721 payload")
+	Attribute("tokenId", String, "Token ID")
+	Attribute("badgeId", String, "Badge ID", func() {
+		Pattern("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$")
+		Example("9302608f-f6a4-4004-b088-63e5fb43cc26")
+	})
+	Attribute("userId", String, "User ID", func() {
+		Pattern("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$")
+		Example("9302608f-f6a4-4004-b088-63e5fb43cc26")
+	})
+	Required(
+		"tokenId",
+		"badgeId",
+		"userId",
+	)
+})
+
 // WalletPayload is the payload for updating a user's wallet
 var WalletPayload = Type("WalletPayload", func() {
 	Description("Wallet payload")
