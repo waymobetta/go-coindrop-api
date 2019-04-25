f6b9484385e0e9e243f003e6cf4402e244471116
waymobetta <jbroethke@gmail.com>
2019-04-25 09:16:08 -0700

    [design/payloads] add walletAddress field to assign ERC721 payload

--- design/payloads.go
+++ design/payloads.go
@@ -64,9 +64,14 @@ var AssignERC721Payload = Type("AssignERC721Payload", func() {
 		Pattern("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$")
 		Example("9302608f-f6a4-4004-b088-63e5fb43cc26")
 	})
+	Attribute("walletAddress", String, "Wallet address", func() {
+		Pattern("^0x[0-9a-fA-F]{40}$")
+		Example("0x845fdD93Cca3aE9e380d5556818e6d0b902B977c")
+	})
 	Required(
 		"badgeId",
 		"userId",
+		"walletAddress",
 	)
 })
 
