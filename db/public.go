33f0d009375272f5af7428591e45fdbde49b0481
waymobetta <jbroethke@gmail.com>
2019-04-27 14:49:51 -0700

    [db/public] fix SQL statement to return badges based on reddit username

--- db/public.go
+++ db/public.go
@@ -11,15 +11,12 @@ func (db *DB) GetBadgesByRedditUsername(redditUsername string) ([]*types.PublicB
 
 	var publicBadgeSlice []*types.PublicBadge
 
-	// create SQL statement for db query
 	sqlStatement := `
 		SELECT
 			coindrop_badges.name,
-			coindrop_tasks.author,
 			coindrop_badges.description,
 			coindrop_badges.logo_url,
-			coindrop_badges.erc721_contract_address,
-			coindrop_erc721s.token_id
+			coindrop_tasks.author
 		FROM
 			coindrop_erc721s
 		JOIN
@@ -41,11 +38,7 @@ func (db *DB) GetBadgesByRedditUsername(redditUsername string) ([]*types.PublicB
 			)
 	`
 
-	// execute db query by passing in prepared SQL statement
-	rows, err := db.client.Query(
-		sqlStatement,
-		redditUsername,
-	)
+	rows, err := db.client.Query(sqlStatement, redditUsername)
 	if err != nil {
 		return nil, err
 	}
@@ -54,38 +47,30 @@ func (db *DB) GetBadgesByRedditUsername(redditUsername string) ([]*types.PublicB
 
 	// iterate over rows
 	for rows.Next() {
-		// initialize new struct per user in db to hold user info
-		publicBadge := &types.PublicBadge{
-			ERC721: new(types.ERC721),
-		}
+		// initialize new struct per transaction in db to hold transaction info
+		publicBadge := new(types.PublicBadge)
 
 		var (
 			badgeName        sql.NullString
-			badgeProject     sql.NullString
 			badgeDescription sql.NullString
 			badgeLogoURL     sql.NullString
-			contractAddress  sql.NullString
-			tokenId          sql.NullString
+			badgeProject     sql.NullString
 		)
 
 		err = rows.Scan(
 			&badgeName,
-			&badgeProject,
 			&badgeDescription,
 			&badgeLogoURL,
-			&contractAddress,
-			&tokenId,
+			&badgeProject,
 		)
 		if err != nil {
 			return nil, err
 		}
 
 		publicBadge.Name = badgeName.String
-		publicBadge.Project = badgeProject.String
 		publicBadge.Description = badgeDescription.String
 		publicBadge.LogoURL = badgeLogoURL.String
-		publicBadge.ERC721.ContractAddress = contractAddress.String
-		publicBadge.ERC721.TokenID = tokenId.String
+		publicBadge.Project = badgeProject.String
 
 		// append publicBadge object to slice of publicBadgeSlice
 		publicBadgeSlice = append(publicBadgeSlice, publicBadge)
